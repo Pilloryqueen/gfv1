798ba96f8921c73415399f8c1ee9b96a911c2b7e
Pilloryqueen <pilloryqueen@gmail.com>
2026-05-07 15:51:47 +0200

    fix: playbookname bug

--- module/documents/gfv1Actor.mjs
+++ module/documents/gfv1Actor.mjs
@@ -1,3 +1,5 @@
+import Playbook from "../sheets/elements/playbook.mjs";
+
 export default class Gfv1Actor extends Actor {
   async enrichedDescription(secrets) {
     return TextEditor.enrichHTML(this.system.description, {
@@ -25,5 +27,19 @@ export default class Gfv1Actor extends Actor {
     };
     await Item.create(identity, { parent: this });
     await tag.delete();
+  }
+
+  /**
+   * Import a playbook onto character
+   * @param {Playbook} playbook playbook to import
+   * @param {Array} items items from that playbook to import
+   */
+  async importPlaybook(playbook, items) {
+    const playbookType = playbook.system.playbookType;
+    if (!this.system.allowedPlaybookTypes.includes(playbookType)) return;
+
+    await this.system.addItems(items);
+    const nameKey = `system._${playbookType}`;
+    await this.update({ [nameKey]: playbook.name });
   }
 }
--- module/util/dialogHelper.mjs
+++ module/util/dialogHelper.mjs
@@ -40,7 +40,7 @@ export default class DialogHelper {
         },
       });
     } catch {
-      return [];
+      return;
     }
   }
 
